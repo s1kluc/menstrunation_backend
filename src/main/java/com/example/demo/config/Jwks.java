d6bc29ab80dd037faddf2433a8f24f3e83dc643a
lucas.lipis.vema <lucas.lipis@vema-eg.de>
2025-05-27 15:40:16 +0200

    security rausgenommen, einfach docker starten

--- src/main/java/com/example/demo/config/Jwks.java
+++ src/main/java/com/example/demo/config/Jwks.java
@@ -1,35 +1,35 @@
-package com.example.demo.config;
-
-import com.nimbusds.jose.jwk.RSAKey;
-
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPublicKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.util.UUID;
-
-public final class Jwks {
-    private Jwks() {}
-
-    public static RSAKey generateRsa() {
-        KeyPair keyPair = generateRsaKey();
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-
-        return new RSAKey.Builder(publicKey)
-                .privateKey(privateKey)
-                .keyID(UUID.randomUUID().toString())
-                .build();
-    }
-
-    private static KeyPair generateRsaKey() {
-        try {
-            KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
-            keyGenerator.initialize(2048);
-            return keyGenerator.generateKeyPair();
-        } catch (Exception ex) {
-            throw new IllegalStateException("Failed to generate RSA key pair", ex);
-        }
-    }
-}
-
+//package com.example.demo.config;
+//
+//import com.nimbusds.jose.jwk.RSAKey;
+//
+//import java.security.KeyPair;
+//import java.security.KeyPairGenerator;
+//import java.security.interfaces.RSAPublicKey;
+//import java.security.interfaces.RSAPrivateKey;
+//import java.util.UUID;
+//
+//public final class Jwks {
+//    private Jwks() {}
+//
+//    public static RSAKey generateRsa() {
+//        KeyPair keyPair = generateRsaKey();
+//        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+//        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+//
+//        return new RSAKey.Builder(publicKey)
+//                .privateKey(privateKey)
+//                .keyID(UUID.randomUUID().toString())
+//                .build();
+//    }
+//
+//    private static KeyPair generateRsaKey() {
+//        try {
+//            KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+//            keyGenerator.initialize(2048);
+//            return keyGenerator.generateKeyPair();
+//        } catch (Exception ex) {
+//            throw new IllegalStateException("Failed to generate RSA key pair", ex);
+//        }
+//    }
+//}
+//
